ae02caf69b7521f97f8f34e5a43cbbbef9efb78d
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2025-11-21 03:03:04 -0700

    RSS fetcher: Simplify code
    There as a lot of unnecessary error handling code.

--- plugins/builders/rss_fetcher.rb
+++ plugins/builders/rss_fetcher.rb
@@ -4,29 +4,14 @@ require 'uri'
 class Builders::RssFetcher < SiteBuilder
   def build
     hook :site, :pre_render do |site|
-      # Fetch RSS from S3
-      rss_url = 'https://eatalks.s3.us-east-2.amazonaws.com/1755269.rss'
+      uri = URI.parse('https://eatalks.s3.us-east-2.amazonaws.com/1755269.rss')
+      response = Net::HTTP.get_response(uri)
+      rss_content = response.body
 
-      begin
-        uri = URI.parse(rss_url)
-        response = Net::HTTP.get_response(uri)
-
-        if response.is_a?(Net::HTTPSuccess)
-          rss_content = response.body
-
-          # Create a static resource for the RSS feed
-          add_resource :rss, '1755269.rss' do
-            layout :none
-            content rss_content
-            permalink '/1755269.rss'
-          end
-
-          Bridgetown.logger.info "RSS Feed:", "Successfully fetched from S3"
-        else
-          Bridgetown.logger.warn "RSS Feed:", "Failed to fetch from S3 (HTTP #{response.code})"
-        end
-      rescue => e
-        Bridgetown.logger.error "RSS Feed:", "Error fetching from S3: #{e.message}"
+      add_resource :rss, '1755269.rss' do
+        layout :none
+        content rss_content
+        permalink '/1755269.rss'
       end
     end
   end
